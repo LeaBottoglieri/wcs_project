e8eeb55954fb33b9afeeab0b2429d0d7920a83b4
LeaBottoglieri <91319663+LeaBottoglieri@users.noreply.github.com>
2022-05-25 14:53:52 +0200

    First commit

---
+++ Projet_WCS/controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Controllers;
+
+class HomeController
+{
+    public function display()
+    {
+        $model = new \Models\Member();
+        $users = $model->getAllMembers();
+
+        $i = 0;
+        $columns = 3;
+
+        $template = "home.phtml";
+        include_once 'views/layout.phtml';
+    }
+}
